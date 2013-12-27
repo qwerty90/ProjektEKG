3598a4b74d5f42245334efb8dbecc9ddc5fa687e
Tomasz <pasternak.t@gmail.com>
2013-12-27 18:44:54 +0100

    Clang format

--- UnitTests/tst_unitteststest.cpp
+++ UnitTests/tst_unitteststest.cpp
@@ -1,50 +1,44 @@
 #include <QString>
 #include <QtTest>
 #include "../src/rrintervalmethod.h"
-class UnitTestsTest : public QObject
-{
-    Q_OBJECT
+class UnitTestsTest : public QObject {
+  Q_OBJECT
 
 public:
-    UnitTestsTest();
+  UnitTestsTest();
 
 private Q_SLOTS:
-    void countRRIntervalsOneInterval();
-    void countRRIntervalsThreeIntervals();
-    void classifyIntervalsTest();
+  void countRRIntervalsOneInterval();
+  void countRRIntervalsThreeIntervals();
+  void classifyIntervalsTest();
 };
 
-UnitTestsTest::UnitTestsTest()
-{
+UnitTestsTest::UnitTestsTest() {}
+
+void UnitTestsTest::countRRIntervalsOneInterval() {
+  vector<double> RRTime = { 0.1, 0.2 };
+  RRIntervalMethod a;
+  vector<double> intervals = a.countRRInvervals(RRTime);
+  QCOMPARE(*intervals.begin(), 0.1);
+  QCOMPARE(intervals.size(), RRTime.size() - 1);
+}
+void UnitTestsTest::countRRIntervalsThreeIntervals() {
+  vector<double> RRTime = { 0.1, 0.2, 0.5, 0.7, 0.9 };
+  vector<double> ExpIntervals = { 0.2 - 0.1, 0.5 - 0.2, 0.7 - 0.5, 0.9 - 0.7 };
+  RRIntervalMethod a;
+  vector<double> intervals = a.countRRInvervals(RRTime);
+  QCOMPARE(intervals.size(), RRTime.size() - 1);
+  QVERIFY(intervals == ExpIntervals);
 }
 
-void UnitTestsTest::countRRIntervalsOneInterval(){
-    vector<double> RRTime = {0.1,0.2};
-    RRIntervalMethod a;
-    vector<double> intervals = a.countRRInvervals(RRTime);
-    QCOMPARE(*intervals.begin(),0.1);
-    QCOMPARE(intervals.size(),RRTime.size()-1);
+void UnitTestsTest::classifyIntervalsTest() {
+  vector<double> intervals = { 1, 1, 1.5, 0.5 };
+  vector<classification> expectedIntervals = { Regular, Regular, Long, Short };
+  RRIntervalMethod a;
+  a.countAvarageInterval(intervals);
+  vector<classification> classifiedIntervals = a.classifyIntervals(intervals);
+  QVERIFY(classifiedIntervals == expectedIntervals);
 }
-void UnitTestsTest::countRRIntervalsThreeIntervals(){
-    vector<double> RRTime = {0.1,0.2,0.5,0.7,0.9};
-    vector<double> ExpIntervals = {0.2-0.1,0.5-0.2,0.7-0.5,0.9-0.7};
-    RRIntervalMethod a;
-    vector<double> intervals = a.countRRInvervals(RRTime);
-    QCOMPARE(intervals.size(),RRTime.size()-1);
-    QVERIFY(intervals==ExpIntervals);
-}
-
-void UnitTestsTest::classifyIntervalsTest()
-{
-    vector<double> intervals = {1,1,1.5,0.5};
-    vector<classification>expectedIntervals = {Regular,Regular,Long,Short};
-    RRIntervalMethod a;
-    a.countAvarageInterval(intervals);
-    vector<classification>classifiedIntervals = a.classifyIntervals(intervals);
-    QVERIFY(classifiedIntervals==expectedIntervals);
-}
-
-
 
 QTEST_APPLESS_MAIN(UnitTestsTest)
 
--- src/rrintervalmethod.h
+++ src/rrintervalmethod.h
@@ -2,23 +2,21 @@
 #include <vector>
 using std::vector;
 
-enum classification
-{
-    Short,
-    Regular,
-    Long
+enum classification {
+  Short,
+  Regular,
+  Long
 };
-class RRIntervalMethod
-{
-    double avarageInterval;
-    double  markovTable[3][3];
+class RRIntervalMethod {
+  double avarageInterval;
+  double markovTable[3][3];
+
 public:
-    vector<double> countRRInvervals(vector<double> RRtime);
-    vector<classification> classifyIntervals(vector<double> RRIntervals);
-    void countAvarageInterval(vector<double> RRIntervals);
-    void countTransitions(vector<classification> classifiedIntervals);
-    void normalizeMarkovTable();
+  vector<double> countRRInvervals(vector<double> RRtime);
+  vector<classification> classifyIntervals(vector<double> RRIntervals);
+  void countAvarageInterval(vector<double> RRIntervals);
+  void countTransitions(vector<classification> classifiedIntervals);
+  void normalizeMarkovTable();
 
-    RRIntervalMethod();
+  RRIntervalMethod();
 };
-
