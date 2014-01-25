e78670ef3628246ac5246073a279bfd8a38fa3a1
ziemo <ziemoracho@gmail.com>
2014-01-25 19:36:46 +0100

    added QT_DISP

---
+++ EKG/QT_DISP/test.cpp
@@ -0,0 +1,16 @@
+#include "QT_DISP.h"
+
+using namespace std;
+
+int main()
+{
+	
+	string testy ="C:\\Users\\Krzysiek\\Desktop\\medyczne\\medyczne 20.01\\medyczne\\test01.txt";
+	QT_DISP QT_DISP1;
+	QT_DISP1.getInput(testy);
+	QT_DISP1.Run();
+
+	return 0;
+}
+
+	
